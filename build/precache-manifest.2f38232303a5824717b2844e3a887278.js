35a4bc987b50e5bf9d6a97d66d1ab8641a6854fb
Thanawat Keattikulkangwan <bangbala05@gmail.com>
2020-09-09 20:38:55 +0700

    text

---
+++ build/precache-manifest.2f38232303a5824717b2844e3a887278.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b45ffe5c9aac76f6f890921a9eebcc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db7574cb44c771ec7caf",
+    "url": "/static/css/main.4470b84c.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7574cb44c771ec7caf",
+    "url": "/static/js/main.fb1aad65.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "39cba59a48ffa6eea39a5d5f9ec63df6",
+    "url": "/static/media/Inconsolata.39cba59a.ttf"
+  },
+  {
+    "revision": "9f720a55ebc8ddf98c2593757aaa959c",
+    "url": "/static/media/Scarecrow.9f720a55.png"
+  },
+  {
+    "revision": "52eb73dbce8bf4083a59acc2e91699ad",
+    "url": "/static/media/SpaceMono.52eb73db.ttf"
+  }
+]);
